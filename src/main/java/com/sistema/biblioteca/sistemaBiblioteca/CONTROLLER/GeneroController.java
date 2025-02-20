10b49551350c40d15fe3b2b09ecd69979cb74025
Gustavo Stinghen <gustavobr.developer@gmail.com>
2025-02-19 21:38:33 -0300

    Swagger - Usuário, Livro, Genero
    Marcado todas as controllers com as anotações do Swagger. Nesse commit foram adicionadas commits as controllers de usuário, livro e gênero

--- src/main/java/com/sistema/biblioteca/sistemaBiblioteca/CONTROLLER/GeneroController.java
+++ src/main/java/com/sistema/biblioteca/sistemaBiblioteca/CONTROLLER/GeneroController.java
@@ -4,7 +4,16 @@ import com.sistema.biblioteca.sistemaBiblioteca.MODELS.DTO.REQUEST.GENERO.Genero
 import com.sistema.biblioteca.sistemaBiblioteca.MODELS.DTO.REQUEST.GENERO.GeneroRequestDTO;
 import com.sistema.biblioteca.sistemaBiblioteca.MODELS.DTO.RESPONSE.GENERO.GeneroFullResponseDTO;
 import com.sistema.biblioteca.sistemaBiblioteca.SERVICE.GeneroService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -37,8 +46,16 @@ public class GeneroController {
      * @return Um ResponseEntity contendo o genero criado e o status HTTP 201 (Created) ou o status HTTP 400 (Bad Request).
      * @see GeneroService#criarGenero(GeneroRequestDTO), GeneroRequestDTO, GeneroFullResponseDTO
      */
+    @Tag( name = "Genero", description = "Recurso para gerenciamento de generos" )
+    @Operation( summary = "Criar um novo genero", description = "Endpoint para criar um novo genero" )
+    @ApiResponse( responseCode = "201", description = "Genero criado com sucesso",
+            content = @Content( schema = @Schema( implementation = GeneroFullResponseDTO.class ) , examples = @ExampleObject( value = "{ \"id\": 1, \"nome\": \"Romance\", \"descricao\": \"Livros de romance\" }" ) ))
+    @ApiResponse( responseCode = "400", description = "Erro ao criar genero" )
+    @ApiResponse( responseCode = "500", description = "Erro interno do servidor" )
+    @SecurityRequirement( name = "Bearer" )
     @PostMapping
-    public ResponseEntity<GeneroFullResponseDTO> criarGenero ( @RequestBody @Valid GeneroRequestDTO generoRequestDTO ) {
+    public ResponseEntity<GeneroFullResponseDTO> criarGenero (
+            @RequestBody @Parameter( description = "Dados do genero", required = true, example = "{ \"nome\": \"Romance\", \"descricao\": \"Livros de romance\" }", content = @Content( schema = @Schema( implementation = GeneroRequestDTO.class ) ) ) @Valid GeneroRequestDTO generoRequestDTO ) {
         try {
             GeneroFullResponseDTO generoFullResponseDTO = service.criarGenero( generoRequestDTO );
             return new ResponseEntity<>( generoFullResponseDTO, HttpStatus.CREATED );
@@ -55,8 +72,17 @@ public class GeneroController {
      * @return Um ResponseEntity contendo o genero atualizado e o status HTTP 200 (OK) ou o status HTTP 400 (Bad Request).
      * @see GeneroService#atualizarGenero(Integer, GeneroPutRequestDTO), GeneroPutRequestDTO, GeneroFullResponseDTO
      */
+    @Tag( name = "Genero", description = "Recurso para gerenciamento de generos" )
+    @Operation( summary = "Atualizar um genero", description = "Endpoint para atualizar um genero" )
+    @ApiResponse( responseCode = "200", description = "Genero atualizado com sucesso",
+        content = @Content( schema = @Schema( implementation = GeneroFullResponseDTO.class ) , examples = @ExampleObject( value = "{ \"id\": 1, \"nome\": \"Romance\", \"descricao\": \"Livros de romance\" }" ) ))
+    @ApiResponse( responseCode = "400", description = "Erro ao atualizar genero" )
+    @ApiResponse( responseCode = "500", description = "Erro interno do servidor" )
+    @SecurityRequirement( name = "Bearer" )
     @PutMapping
-    public ResponseEntity<GeneroFullResponseDTO> atualizarGenero (@RequestBody @Valid GeneroPutRequestDTO generoPutRequestDTO, @RequestParam Integer id ) {
+    public ResponseEntity<GeneroFullResponseDTO> atualizarGenero (
+            @RequestBody @Parameter ( description = "Dados do genero", required = true, example = "{ \"nome\": \"Romance\", \"descricao\": \"Livros de romance\" }", content = @Content( schema = @Schema( implementation = GeneroPutRequestDTO.class ) ) ) @Valid GeneroPutRequestDTO generoPutRequestDTO,
+            @RequestParam @Parameter ( description = "ID do genero", required = true, example = "1") Integer id ) {
         try {
             GeneroFullResponseDTO generoFullResponseDTO = service.atualizarGenero( id, generoPutRequestDTO);
             return new ResponseEntity<>( generoFullResponseDTO, HttpStatus.OK );
@@ -70,6 +96,12 @@ public class GeneroController {
      * @return Um ResponseEntity contendo a lista de generos e o status HTTP 200 (OK) ou o status HTTP 404 (Not Found).
      * @see GeneroService#listarGeneros(), GeneroFullResponseDTO
      */
+    @Tag( name = "Genero", description = "Recurso para gerenciamento de generos" )
+    @Operation( summary = "Listar todos os generos", description = "Endpoint para listar todos os generos" )
+    @ApiResponse( responseCode = "200", description = "Generos listados com sucesso",
+        content = @Content( schema = @Schema( implementation = GeneroFullResponseDTO.class ) , examples = @ExampleObject( value = "{ \"id\": 1, \"nome\": \"Romance\", \"descricao\": \"Livros de romance\" }" ) ))
+    @ApiResponse( responseCode = "404", description = "Nenhum genero encontrado" )
+    @ApiResponse( responseCode = "500", description = "Erro interno do servidor" )
     @GetMapping
     public ResponseEntity<List<GeneroFullResponseDTO>> listarGeneros () {
         try {
@@ -85,8 +117,14 @@ public class GeneroController {
      * @return Um ResponseEntity contendo o genero buscado e o status HTTP 200 (OK) ou o status HTTP 404 (Not Found).
      * @see GeneroService#buscarGenero(Integer), GeneroFullResponseDTO
      */
+    @Tag( name = "Genero", description = "Recurso para gerenciamento de generos" )
+    @Operation( summary = "Busca um genero pelo ID", description = "Busca um genero pelo ID e retorna o genero com o status HTTP 200" )
+    @ApiResponse( responseCode = "200", description = "Genero encontrado com sucesso",
+        content = @Content( schema = @Schema( implementation = GeneroFullResponseDTO.class ) , examples = @ExampleObject( value = "{ \"id\": 1, \"nome\": \"Romance\", \"descricao\": \"Livros de romance\" }" ) ))
+    @ApiResponse( responseCode = "404", description = "Genero nao encontrado" )
+    @ApiResponse( responseCode = "500", description = "Erro interno do servidor" )
     @GetMapping("/{id}")
-    public ResponseEntity<GeneroFullResponseDTO> buscarGenero ( @PathVariable @Positive Integer id ) {
+    public ResponseEntity<GeneroFullResponseDTO> buscarGenero ( @PathVariable @Positive @NotNull @Parameter( description = "ID do genero", required = true, example = "1" ) Integer id ) {
         try {
             GeneroFullResponseDTO generoFullResponseDTO = service.buscarGenero( id );
             return new ResponseEntity<>( generoFullResponseDTO, HttpStatus.OK );
@@ -101,6 +139,12 @@ public class GeneroController {
      * @return Um ResponseEntity contendo o status HTTP 200 (OK) ou o status HTTP 400 (Bad Request).
      * @see GeneroService#deletarGenero(Integer)
      */
+    @Tag( name = "Genero", description = "Recurso para gerenciamento de generos" )
+    @Operation( summary = "Deletar um genero", description = "Endpoint para deletar um genero" )
+    @ApiResponse( responseCode = "200", description = "Genero deletado com sucesso" )
+    @ApiResponse( responseCode = "400", description = "Erro ao deletar genero" )
+    @ApiResponse( responseCode = "500", description = "Erro interno do servidor" )
+    @SecurityRequirement( name = "Bearer" )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarGenero ( @PathVariable @Positive Integer id ) {
         try {
--- src/main/java/com/sistema/biblioteca/sistemaBiblioteca/CONTROLLER/UsuarioController.java
+++ src/main/java/com/sistema/biblioteca/sistemaBiblioteca/CONTROLLER/UsuarioController.java
@@ -4,7 +4,16 @@ import com.sistema.biblioteca.sistemaBiblioteca.MODELS.DTO.REQUEST.USUARIO.Usuar
 import com.sistema.biblioteca.sistemaBiblioteca.MODELS.DTO.REQUEST.USUARIO.UsuarioRequestDTO;
 import com.sistema.biblioteca.sistemaBiblioteca.MODELS.DTO.RESPONSE.USUARIO.UsuarioFullResponseDTO;
 import com.sistema.biblioteca.sistemaBiblioteca.SERVICE.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -37,8 +46,16 @@ public class UsuarioController {
      * @return Um ResponseEntity contendo o usuario criado e o status HTTP 201 (Created) ou o status HTTP 400 (Bad Request).
      * @see UsuarioService#criarUsuario(UsuarioRequestDTO), UsuarioRequestDTO, UsuarioFullResponseDTO
      */
+    @Tag( name = "Usuario", description = "Recurso para gerenciamento de usuarios" )
+    @Operation( summary = "Criar um usuario", description = "Endpoint para criar um usuario" )
+    @ApiResponse ( responseCode = "201", description = "Usuario criado com sucesso", content = @Content ( schema = @Schema( implementation = UsuarioFullResponseDTO.class ),
+            examples = @ExampleObject( value = "{ \"id\": 1, \"nome\": \"Gustavo Stinghen\", \"email\": \"tX2Xu@example.com\", \"senha\": \"12345678\" }" ) ) )
+    @ApiResponse( responseCode = "400", description = "Erro ao criar usuario" )
+    @ApiResponse( responseCode = "500", description = "Erro interno do servidor" )
+    @SecurityRequirement( name = "Bearer" )
     @PostMapping
-    public ResponseEntity<UsuarioFullResponseDTO> criarUsuario ( @RequestBody @Valid UsuarioRequestDTO usuarioRequestDTO ) {
+    public ResponseEntity<UsuarioFullResponseDTO> criarUsuario (
+            @RequestBody @Parameter ( required = true, example = "{ \"nome\": \"Gustavo Stinghen\", \"email\": \"tX2Xu@example.com\", \"senha\": \"12345678\" }", content = @Content ( schema = @Schema ( implementation = UsuarioRequestDTO.class ) ) , description = "Usuario a ser criado" ) @Valid UsuarioRequestDTO usuarioRequestDTO ) {
 
         try {
             UsuarioFullResponseDTO usuarioFullResponseDTO = service.criarUsuario( usuarioRequestDTO );
@@ -55,8 +72,17 @@ public class UsuarioController {
      * @return Um ResponseEntity contendo o usuario atualizado e o status HTTP 200 (OK) ou o status HTTP 400 (Bad Request).
      * @see UsuarioService#atualizarUsuario(UsuarioPutRequestDTO, Integer), UsuarioPutRequestDTO, UsuarioFullResponseDTO
      */
+    @Tag( name = "Usuario", description = "Recurso para gerenciamento de usuarios" )
+    @Operation( summary = "Atualizar um usuario", description = "Endpoint para atualizar um usuario" )
+    @ApiResponse ( responseCode = "200", description = "Usuario atualizado com sucesso", content = @Content ( schema = @Schema( implementation = UsuarioFullResponseDTO.class ),
+            examples = @ExampleObject( value = "{ \"id\": 1, \"nome\": \"Gustavo Stinghen\", \"email\": \"tX2Xu@example.com\", \"senha\": \"12345678\" }" ) ) )
+    @ApiResponse( responseCode = "400", description = "Erro ao atualizar usuario" )
+    @ApiResponse( responseCode = "500", description = "Erro interno do servidor" )
+    @SecurityRequirement( name = "Bearer" )
     @PutMapping
-    public ResponseEntity<UsuarioFullResponseDTO> atualizarUsuario ( @RequestBody @Valid UsuarioPutRequestDTO usuarioPutRequestDTO, @RequestParam Integer id ) {
+    public ResponseEntity<UsuarioFullResponseDTO> atualizarUsuario (
+            @RequestBody @Parameter ( required = true, description = "Usuario a ser atualizado", example = "{ \"nome\": \"Gustavo Stinghen\", \"email\": \"tX2Xu@example.com\", \"senha\": \"12345678\" }", content = @Content ( schema = @Schema ( implementation = UsuarioPutRequestDTO.class ) ) ) @Valid UsuarioPutRequestDTO usuarioPutRequestDTO,
+            @RequestParam @Parameter ( required = true, example = "1", description = "ID do usuario a ser atualizado" ) Integer id ) {
 
         try {
             UsuarioFullResponseDTO usuarioFullResponseDTO = service.atualizarUsuario( usuarioPutRequestDTO, id );
@@ -71,6 +97,12 @@ public class UsuarioController {
      * @return Um ResponseEntity contendo a lista de usuarios e o status HTTP 200 (OK) ou o status HTTP 404 (Not Found).
      * @see UsuarioService#listarUsuarios(), UsuarioFullResponseDTO
      */
+    @Tag( name = "Usuario", description = "Recurso para gerenciamento de usuarios" )
+    @Operation( summary = "Listar todos os usuarios", description = "Endpoint para listar todos os usuarios" )
+    @ApiResponse ( responseCode = "200", description = "Usuarios listados com sucesso", content = @Content ( schema = @Schema( implementation = UsuarioFullResponseDTO.class ),
+            examples = @ExampleObject( value = "{ \"id\": 1, \"nome\": \"Gustavo Stinghen\", \"email\": \"tX2Xu@example.com\", \"senha\": \"12345678\" }" ) ) )
+    @ApiResponse( responseCode = "404", description = "Nenhum usuario encontrado" )
+    @ApiResponse( responseCode = "500", description = "Erro interno do servidor" )
     @GetMapping
     public ResponseEntity<List<UsuarioFullResponseDTO>> listarUsuarios () {
 
@@ -87,8 +119,15 @@ public class UsuarioController {
      * @return Um ResponseEntity contendo o usuario buscado e o status HTTP 200 (OK) ou o status HTTP 404 (Not Found).
      * @see UsuarioService#buscarUsuario(Integer), UsuarioFullResponseDTO
      */
+    @Tag( name = "Usuario", description = "Recurso para gerenciamento de usuarios" )
+    @Operation( summary = "Busca um usuario pelo ID", description = "Busca um usuario pelo ID e retorna o usuario com o status HTTP 200" )
+    @ApiResponse( responseCode = "200", description = "Usuario encontrado com sucesso", content = @Content ( schema = @Schema( implementation = UsuarioFullResponseDTO.class ),
+            examples = @ExampleObject( value = "{ \"id\": 1, \"nome\": \"Gustavo Stinghen\", \"email\": \"tX2Xu@example.com\", \"senha\": \"12345678\" }" ) ) )
+    @ApiResponse( responseCode = "404", description = "Usuario nao encontrado" )
+    @ApiResponse( responseCode = "500", description = "Erro interno do servidor" )
     @GetMapping("/{id}")
-    public ResponseEntity<UsuarioFullResponseDTO> buscarUsuario ( @PathVariable @Positive Integer id ) {
+    public ResponseEntity<UsuarioFullResponseDTO> buscarUsuario (
+            @PathVariable @Parameter ( required = true, example = "1", description = "ID do usuario a ser buscado" ) @NotNull @Positive Integer id ) {
 
         try {
             UsuarioFullResponseDTO usuarioFullResponseDTO = service.buscarUsuario( id );
@@ -104,8 +143,13 @@ public class UsuarioController {
      * @return Um ResponseEntity contendo o status HTTP 200 (OK) ou o status HTTP 400 (Bad Request).
      * @see UsuarioService#deletarUsuario(Integer)
      */
+    @Tag( name = "Usuario", description = "Recurso para gerenciamento de usuarios" )
+    @Operation( summary = "Deleta um usuario pelo ID", description = "Deleta um usuario pelo ID e retorna o status HTTP 200" )
+    @ApiResponse( responseCode = "200", description = "Usuario deletado com sucesso" )
+    @ApiResponse( responseCode = "400", description = "Nao foi possivel deletar o usuario" )
+    @ApiResponse( responseCode = "500", description = "Erro interno do servidor" )
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletarUsuario ( @PathVariable @Positive Integer id ) {
+    public ResponseEntity<Void> deletarUsuario ( @PathVariable @Parameter ( required = true, example = "1", description = "ID do usuario a ser deletado" ) @NotNull @Positive Integer id ) {
 
         try {
             service.deletarUsuario( id );
